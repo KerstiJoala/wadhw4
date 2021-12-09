e043b7257b05349f0f8436eb3ec6cc96a7273e91
KerstiJoala <kerstij@gmail.com>
2021-12-09 14:28:12 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -1,74 +1,94 @@
 const express = require('express');
 const pool = require('./database');
+const cors = require('cors');
 const app = express();
+// register the ejs view engine
 app.set('view engine', 'ejs');
-app.use(express.json());
-app.listen(8080, () => {
-    console.log("Server is listening to port 8080")
-});
+//without this middleware, we cannot use data submitted by forms
+app.use(express.urlencoded({extended: true}));
 
-app.get('/posts', async(req, res) => {
+app.use(express.json());
+app.use(cors());
+app.use(express.static('Public'));
+app.listen(3000, () => {
+    console.log("Server is listening to port 3000")
+});
+app.get('/', async (req, res) => {
     try {
         console.log("get posts request has arrived");
         const posts = await pool.query(
             "SELECT * FROM posts"
         );
-        res.json(posts.rows);
+        res.render('posts', {posts: posts.rows});
     } catch (err) {
         console.error(err.message);
     }
 });
-
-
-app.get('/posts/:id', async(req, res) => {
+app.get('/posts', async (req, res) => {
     try {
-        console.log("get a post request has arrived");
+        console.log("get posts request has arrived");
+        const posts = await pool.query(
+            "SELECT * FROM posts"
+        );
+        res.render('posts', {posts: posts.rows});
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+app.get('/singlepost/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        console.log(req.params.id);
+        console.log("get a single post request has arrived");
         const posts = await pool.query(
             "SELECT * FROM posts WHERE id = $1", [id]
         );
-        res.json(posts.rows[0]);
+        res.render('singlepost', {posts: posts.rows[0]});
     } catch (err) {
         console.error(err.message);
     }
 });
-
-
-app.post('/posts/', async(req, res) => {
+app.get('/posts/:id', async (req, res) => {
     try {
-        console.log("a post request has arrived");
-        const post = req.body;
-        const newpost = await pool.query(
-            "INSERT INTO posts(title, body, urllink) values ($1, $2, $3) RETURNING*", [post.title, post.body, post.urllink]
-    );
-        res.json( newpost );
+        const {id} = req.params;
+        console.log("get a post request has arrived");
+        const Apost = await pool.query(
+            "SELECT * FROM posts WHERE id = $1", [id]
+        );
+        res.json(Apost.rows[0]);
     } catch (err) {
         console.error(err.message);
     }
 });
-
-
-app.put('/posts/:id', async(req, res) => {
+app.delete('/posts/:id', async (req, res) => {
     try {
-        const { id } = req.params;
-        const post = req.body;
-        console.log("update request has arrived");
-        const updatepost = await pool.query(
-            "UPDATE posts SET (title, body, urllink) = ($2, $3, $4) WHERE id = $1", [id, post.title, post.body, post.urllink]
-    );
-        res.json(post);
-    } catch (err) {
-        console.error(err.message);
-    }
-});
-
-app.delete('/posts/:id', async(req, res) => {
-    try {
-        const { id } = req.params;
+        console.log(req.params);
+        const {id} = req.params;
         const post = req.body;
         console.log("delete a post request has arrived");
-        const deletepost = await pool.query("DELETE FROM posts WHERE id = $1", [id]);
-        res.json(post);
+        const deletepost = await pool.query(
+            "DELETE FROM posts WHERE id = $1", [id]
+        );
+        res.redirect('posts');
     } catch (err) {
         console.error(err.message);
     }
+});
+app.post('/posts', async (req, res) => {
+    try {
+        const post = req.body;
+        console.log(post);
+        const newpost = await pool.query(
+            "INSERT INTO posts(title, body, urllink) values ($1, $2, $3) RETURNING *", [post.title, post.body, post.urllink]
+        );
+        res.redirect("singlepost/" + newpost.rows[0].id);
+    } catch (err) {
+        console.error(err.message)
+    }
+});
+app.get('/addnewpost', (req, res) => {
+    res.render('addnewpost');
+});
+app.use((req, res) => {
+    res.status(404).render('404');
 });
